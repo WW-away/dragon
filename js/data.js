00981227cd1a9fc2b7823cfb6b2f9cceb6da59af
away <away1994@qq.com>
2021-04-23 23:07:58 +0800

    no message

---
+++ js/data.js
@@ -0,0 +1,9 @@
+var jsonData = [
+  { "id": 1, "title": "Alberto Giacometti’s Retrospective", "year": 2016, "date": "3.22-7.31", "add": "Shanghai Yuz Museum" },
+  { "id": 2, "title": "Rain Room by Random International", "year": 2015, "date": "9.1-1.3", "add": "Shanghai Yuz Museum" },
+  { "id": 3, "title": "Random International: Everything & Nothing", "year": 2018, "date": "4.20-9.2", "add": "Shanghai Yuz Museum" },
+  { "id": 4, "title": "CHARLIE CHAPLIN. A VISION", "year": 2018, "date": "6.8-10.7", "add": "Shanghai Yuz Museum" },
+  { "id": 5, "title": "Su-Mei Tse: Nested", "year": 2018, "date": "12.18-3.24（2019）", "add": "Shanghai Yuz Museum" },
+  { "id": 6, "title": "In Production: Art and the Studio System", "year": 2019, "date": "11.7-3.1（2020）", "add": "Shanghai Yuz Museum" },
+  { "id": 7, "title": "Alicja Kwade: ReReason", "year": "", "date": "12.17-4.1", "add": "Shanghai Yuz Museum" }
+]
