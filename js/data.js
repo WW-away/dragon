5c3421956218ddad22d5616c81bf2ba4911482ee
away <away1994@qq.com>
2021-04-24 18:53:54 +0800

    no message

--- js/data.js
+++ js/data.js
@@ -1,9 +1,191 @@
-var jsonData = [
-  { "id": 1, "title": "Alberto Giacometti’s Retrospective", "year": 2016, "date": "3.22-7.31", "add": "Shanghai Yuz Museum" },
-  { "id": 2, "title": "Rain Room by Random International", "year": 2015, "date": "9.1-1.3", "add": "Shanghai Yuz Museum" },
-  { "id": 3, "title": "Random International: Everything & Nothing", "year": 2018, "date": "4.20-9.2", "add": "Shanghai Yuz Museum" },
-  { "id": 4, "title": "CHARLIE CHAPLIN. A VISION", "year": 2018, "date": "6.8-10.7", "add": "Shanghai Yuz Museum" },
-  { "id": 5, "title": "Su-Mei Tse: Nested", "year": 2018, "date": "12.18-3.24（2019）", "add": "Shanghai Yuz Museum" },
-  { "id": 6, "title": "In Production: Art and the Studio System", "year": 2019, "date": "11.7-3.1（2020）", "add": "Shanghai Yuz Museum" },
-  { "id": 7, "title": "Alicja Kwade: ReReason", "year": "", "date": "12.17-4.1", "add": "Shanghai Yuz Museum" }
+var jsonData = [{
+  "id": 1,
+  "title": "Alberto Giacometti’s Retrospective",
+  "year": 2016,
+  "date": "3.22-7.31",
+  "add": "Shanghai Yuz Museum"
+},
+{
+  "id": 2,
+  "title": "Rain Room by Random International",
+  "year": 2015,
+  "date": "9.1-1.3",
+  "add": "Shanghai Yuz Museum"
+},
+{
+  "id": 3,
+  "title": "Random International: Everything & Nothing",
+  "year": 2018,
+  "date": "4.20-9.2",
+  "add": "Shanghai Yuz Museum"
+},
+{
+  "id": 4,
+  "title": "CHARLIE CHAPLIN. A VISION",
+  "year": 2018,
+  "date": "6.8-10.7",
+  "add": "Shanghai Yuz Museum"
+},
+{
+  "id": 5,
+  "title": "Su-Mei Tse: Nested",
+  "year": 2018,
+  "date": "12.18-3.24（2019）",
+  "add": "Shanghai Yuz Museum"
+},
+{
+  "id": 6,
+  "title": "In Production: Art and the Studio System",
+  "year": 2019,
+  "date": "11.7-3.1（2020）",
+  "add": "Shanghai Yuz Museum"
+},
+{
+  "id": 7,
+  "title": "Alicja Kwade: ReReason",
+  "year": "",
+  "date": "12.17-4.1",
+  "add": "Shanghai Yuz Museum"
+},
+{
+  "id": 7,
+  "title": "Dali’s Fantastic Universe",
+  "year": "2015",
+  "date": "9.26-1.10 (2016)",
+  "add": ""
+},
+{
+  "id": 7,
+  "title": "Pierre Auguste RENOIR All a life for painting",
+  "year": "2015",
+  "date": "11.20-3.6 （2016）",
+  "add": ""
+},
+{
+  "id": 7,
+  "title": "Masterpieces from the Centre Pompidou 1906-1977",
+  "year": "2016",
+  "date": "10-1（2017）",
+  "add": ""
+},
+{
+  "id": 7,
+  "title": "Impression Sunrise",
+  "year": "2020",
+  "date": "9.17-1.3 （2021）",
+  "add": ""
+},
+{
+  "id": 7,
+  "title": "“What About Art?” Contemporary Art from China Exhibition",
+  "year": "2016",
+  "date": "3.15-6.30",
+  "add": "AI Riwaq Museum in Doha Qatar"
+},
+{
+  "id": 7,
+  "title": "Super Studio: 50",
+  "year": "2017",
+  "date": "12.16-3.11",
+  "add": "Power Station of Art"
+},
+{
+  "id": 7,
+  "title": "The 11th Shanghai Biennale",
+  "year": "",
+  "date": "",
+  "add": ""
+}
+]
+
+var servicesData = [{
+  "id": 1,
+  "title": "Packing & crating",
+  "images": ["images/service-1.jpg", "images/service-5.jpg", "images/service-8.jpg"],
+  "p": [
+    "Dragon Fine Art provides different levels of packing and crating using various packing materials. All soft packing materials meet corresponding GB standards and all crates are ISPM15 compliant, qualified for international shipping.",
+    "From simple soft packing for local collection and transfer, economy one-way or two-way crates for domestic transport, travelling frame for easy moving and unpacking to slot crates, foam core crates and museum standard thermo crates, Dragon Fine Art offers a full range of solutions that will suite the clients’ need."
+  ]
+},
+{
+  "id": 2,
+  "title": "Installation and Rigging",
+  "images": ["images/service-1.jpg", "images/service-5.jpg", "images/service-8.jpg"],
+  "p": [
+    "The Dragon Fine Art installation team consists of highly skilled art handlers for installing and deinstalling artworks at art museums, art fairs, galleries and collectors’ home. The team is equipped with state-of-the-art tools to cope with any challenge on site.",
+    "The Dragon Fine Art rigging team is dedicated to handling oversized and overweight sculptures and installations. All members are veterans specialized in heavy lifting and assembling jobs.",
+    "Prior to any complex jobs, a site survey will be made to come up with a working plan or even a demonstration to ensure that the task will be performed in a safe manner.",
+    "The rigging team has at its disposal all necessary tools and equipment to carry out the jobs. Electric tacker, gantry, chain hoist, slings, J-bar, heavy duty pallet jack are some among the available. Tailor-made equipment/tool is also possible if required."
+  ]
+},
+{
+  "id": 3,
+  "title": "Road transport",
+  "images": ["images/service-1.jpg", "images/service-5.jpg", "images/service-8.jpg"],
+  "p": [
+    "Dragon Fine Art is an authorized road transport service provider by the Chinese government. All trucks are equipped with air-ride suspension, climate-control system, hydraulic tail lift, satellite tracking and security monitoring system to give maximum safety and care to the artworks being transported. Dual-drivers is a must on any Dragon Fine Art truck.",
+    "Road transport covers all areas in China except Xinjiang and Tibet provinces. Whether it is a transfer between the airport and the city or a long-distance haul from one city to another, Dragon Fine Art offers dedicated trucking as well as consolidated service. The current art shuttle between Shanghai and Beijing runs two times, Tuesday and Friday, every week."
+  ]
+},
+{
+  "id": 4,
+  "title": "Air & sea shipping",
+  "images": ["images/service-1.jpg", "images/service-5.jpg", "images/service-8.jpg"],
+  "p": [
+    "Dragon Fine Art works with various carriers to provide the clients air/sea shipping to any destination around the world. An extensive overseas agent network ensures that the shipment can always be delivered safely to the clients’ door when required.",
+    "For air shipping, Dragon Fine Art can offer consolidated or exclusive load, both applicable to full airport supervision (including palletizing and depalletizing monitoring, tarmac escort) upon request. Airside monitoring is also available for connecting flights.",
+    "For sea shipping, Dragon Fine Art can book space as LCL or FCL on any type of sea container, be it dry containers, reefers or out-of-gauge flat rack containers."
+  ]
+},
+{
+  "id": 5,
+  "title": "Airport supervision",
+  "images": ["images/service-1.jpg", "images/service-5.jpg", "images/service-8.jpg"],
+  "p": [
+    "Upon request, Dragon Fine Art can carry out full airport supervision at departure, connection and arrival at any destination in the world. A real-time update of the supervision process will be recorded and notified to the client from pallet buildup until flight wheels-up for departure or from aircraft cargo-door open until pallet breakdown."
+  ]
+},
+{
+  "id": 6,
+  "title": "Customs formalities",
+  "images": ["images/service-1.jpg", "images/service-5.jpg", "images/service-8.jpg"],
+  "p": [
+    "Dragon Fine Art boasts its capability to outperform other players in China in terms of customs clearance for any shipment, be it temporary import or permanent import. The Dragon Fine Art customs broker is proud to be one of the few who can exempt customs bond for any inbound shipment for exhibition in China under a valid exhibition permit.",
+    "Flexible customs/quarantine inspections can be arranged to cater to the different natures or flight schedules to maintain maximum safety of the artworks being shipped."
+  ]
+},
+{
+  "id": 7,
+  "title": "Bonded warehouse",
+  "images": ["images/service-1.jpg", "images/service-5.jpg", "images/service-8.jpg"],
+  "p": [
+    "Dragon Fine Art operates a 500 sqm warehouse in the Shanghai Customs Bonded Zone. It allows international shipments to be transferred directly to the warehouse after arrival without any import/exhibition permit. The shipment can either be stored there or be customs cleared under a valid permit and be released from the Customs Bonded Zone for display purpose with 6-month limit, or for permanent import after import tax and VAT is paid."
+  ]
+},
+{
+  "id": 8,
+  "title": "Storage",
+  "images": ["images/service-1.jpg", "images/service-5.jpg", "images/service-8.jpg"],
+  "p": [
+    "The Dragon Fine Art storage is located just 15km away from the city center, a 40-minute drive by truck at time of the day. With 400 sqm of ambient temperature area and 150 sqm of climate-control area, the warehouse can accommodate regular storage as well as museum standard storage with temperature and humidity control.",
+    "The climate-control area is fitted with security cameras, both fixed angle view and panoramic view, infrared motion detectors that are linked to the security control room, manned 24-7. A palm-print system makes sure that only the authorized personnel can have access to this area.",
+    "The ambient storage area is also covered by security cameras and infrared and intrusion alarm linked to the security control room manned 24-7."
+  ]
+},
+{
+  "id": 9,
+  "title": "License & permit in China",
+  "images": ["images/service-1.jpg", "images/service-5.jpg", "images/service-8.jpg"],
+  "p": [
+    "Exhibition license/import permit is key to shipping artworks into and out of China. Dragon Fine Art can assist the clients applying for an appropriate license/permit at a reasonable cost to facilitate the import and export formalities, be it temporary or definitive. Full details of the artwork shall be required prior to any application."
+  ]
+},
+{
+  "id": 10,
+  "title": "Insurance",
+  "images": ["images/service-1.jpg", "images/service-5.jpg", "images/service-8.jpg"],
+  "p": [
+    "Dragon Fine Art works with trusted brokers to seal deals with insurance companies at amazingly competitive rates. The clients can free themselves of the back-and-forth with the insurance companies when there is an issue with the artwork."
+  ]
+},
 ]
